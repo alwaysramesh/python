2b527ac77e81425e04e2dcce776ff1d278faf253
alwaysramesh <viprameshkumar8085@gmail.com>
2024-10-29 20:26:18 +0530

    MCQ Question
    git add .

---
+++ Codechef291024/MCQ.py
@@ -0,0 +1,35 @@
+'''Role of endl
+What is the role of "\n" in the below code?
+
+print(7, "\n", 5)'''
+'''
+Select one of the following options:
+
+Subtraction
+Addition
+Division
+None of the above
+Great job, keep it up!
+
+Correct Answer:
+
+None of the above
+Explanation:
+"\n" is used to move to a new line
+'''
+#2nd Question
+
+'''Print Many Literals - MCQ
+What will be the output of the following code snippet?
+
+print("Hello", "World", 5, "endl", 2) 
+
+Select one of the following options:
+Hello World 5 endl 2
+Hello World5 2
+Hello World 5 2
+Hello World 5 end1 2'''
+
+#correct Option
+#  Hello World 5 endl 2
+
