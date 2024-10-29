39d3b867ed0bd5a750b446eb38266d5f1bf046cc
alwaysramesh <viprameshkumar8085@gmail.com>
2024-10-29 19:59:02 +0530

    MCQ Question

---
+++ Codechef291024/PrintLine.py
@@ -0,0 +1,20 @@
+'''Output Format - MCQ
+What does the following code achieve?
+
+print(8)
+print(13, end=" ")
+print(21) 
+
+Select one of the following options:
+
+1) Prints three numbers on separate lines
+2) Prints three numbers on the same line
+3) Prints one number on the first line, two numbers on the next line
+4) Throws a compilation error '''
+'''See Answer
+ Correct Answer:
+
+Prints one number on the first line, two numbers on the next line
+Explanation:
+end=" " is used to not skip to a new line'''
+#3) Prints one number on the first line, two numbers on the next line
