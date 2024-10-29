2fbda74610f2248dbb6b13bdf9ee4f1fe1405edf
alwaysramesh <viprameshkumar8085@gmail.com>
2024-10-29 20:16:52 +0530

    With Space

---
+++ Codechef291024/spacePrint.py
@@ -0,0 +1,19 @@
+'''Print Python Programming
+To print "Python" and "Programming" on the same line with a space in between, you would write:'''
+'''Select one of the following options:
+print("PythonProgramming")
+print("Python  ","Programming")
+print("Python","Programming")
+print("Python")
+print("Programming")
+Excellent work!
+
+Correct Answer:
+
+print("Python","Programming")
+Explanation:
+"," is used to separate multiple outputs on the same line inside the print statement
+
+Please note that 2nd and 3rd option are not same. Second option already has trailing space in the first string. So it will print two spaces between the strings.'''
+#correct Option
+#print("Python","Programming")
