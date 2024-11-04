46263a31f78637003b55b6f830c98e060b2c6774
alwaysramesh <viprameshkumar8085@gmail.com>
2024-11-04 19:54:48 +0530

    04102024

---
+++ Codechef291024/MCQ04102024.py
@@ -0,0 +1,14 @@
+'''Loop and Print Sequence - MCQ
+What does the following code snippet do?
+
+count = 0
+while count < 5:
+    print(count, end=" ")
+    count += 1
+
+    Select one of the following options:
+01) Prints numbers 0 to 4 with spaces
+02) Prints numbers 1 to 5 with spaces
+03) Prints numbers 0 to 4 without spaces
+04) Prints numbers 1 to 5 without spaces'''
+#Correct:- Prints numbers 0 to 4 with spaces
