5af72b790dd8bfb4e54864a7c452dbaf673671c1
alwaysramesh <viprameshkumar8085@gmail.com>
2024-11-04 20:04:12 +0530

    MCQ

--- Codechef291024/MCQ04102024.py
+++ Codechef291024/MCQ04102024.py
@@ -12,3 +12,19 @@ while count < 5:
 03) Prints numbers 0 to 4 without spaces
 04) Prints numbers 1 to 5 without spaces'''
 #Correct:- Prints numbers 0 to 4 with spaces
+
+#Que.00
+'''While Loop Increment - MCQ
+What is the output of the following code snippet?
+
+x = 0
+while x < 5:
+    print(x, end=" ")
+    x += 2
+    
+    Select one of the following options:
+01) 0 2 4 6
+02) 0 2 4
+03) 2 4 6
+04) 0 2 4 6 8'''
+#correct= 0 2 4
